5ebfaef17310d477937b17b2d983bf4cbda9cd20
ButenkoT <tanya@butenko.me>
2015-07-13 12:49:10 +1000

    panorama is done

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
 var app = app || {};
+//var Detector = require('detector.js');
 
-app.init = function(){
+app.init = function () {
 
   // There are a couple of things that we need for any THREE.js page.
-  // A camera
-  // A scene
-  // A renderer
+  // A camera, a scene and a renderer
 
-  //assign width and height once, so it doesnt need to calculate it each time
   app.width = window.innerWidth;
   app.height = window.innerHeight;
 
   //(field of view, ration, near, far )
   //near and far is a range from how close it ll be and how far from the screen
-  app.camera = new THREE.PerspectiveCamera(45, app.width/app.height, 1, 1000 );
+  app.camera = new THREE.PerspectiveCamera(100, app.width / app.height, 1, 1000);
   app.camera.position.z = 200;
 
 
@@ -22,41 +20,35 @@ app.init = function(){
   app.scene.add(app.camera);
 
 
+  //checks if webgl is not supported replaces it with canvas
+  //app.renderer = Detector.webgl ? new THREE.WebGLRenderer() : new THREE.CanvasRenderer();
   app.renderer = new THREE.WebGLRenderer();
-  //setting any size we want it to be here
   app.renderer.setSize(app.width, app.height);
-  app.renderer.setClearColor(0xE3F2FD, 1); //color in hex and transparency
-
-  console.log(app.renderer);
-
-  //orbitControl we add it before we append child, to control elements in z dimention with touchpad
-  app.controls = new THREE.OrbitControls(app.camera, app.renderer.domElement);
 
   document.body.appendChild(app.renderer.domElement);
-  // app.renderer.render(app.scene, app.camera);
 
   app.addSphere();
   app.animate();
 
 };
 
-app.addSphere = function(){
+app.addSphere = function () {
 
-  var shape = new THREE.SphereGeometry(100, 32, 32);
+  var shape = new THREE.SphereGeometry(app.height, 32, 32);
   var material = new THREE.MeshBasicMaterial({
-    //1600x800px image
     map: THREE.ImageUtils.loadTexture('images/bergsjostolen.jpg')
-    //color: 0x1A237E
-    //wireframe: true
   });
 
   app.sphere = new THREE.Mesh(shape, material);
+
+  //shows picture from the inside-out
+  app.sphere.scale.x = -1;
 
   app.scene.add(app.sphere);
 
 };
 
-app.animate = function(){
+app.animate = function () {
 
   requestAnimationFrame(app.animate);
 
@@ -68,14 +60,23 @@ app.animate = function(){
 
 window.onload = app.init;
 
-//window.addEventListener("mousemove", function(event){
-//
-//  app.cube.position.x = event.clientX - (app.width/2);
-//  app.cube.position.y = (app.height/2) - event.clientY;
-//
-//});
+function onMouseWheel(event) {
+  if (event.wheelDeltaY) {                      // WebKit
+    app.camera.fov -= event.wheelDeltaY * 0.05;
+  } else if (event.wheelDelta) {                // Opera / IE9
+    app.camera.fov -= event.wheelDelta * 0.05;
+  } else if (event.detail) {                    // Firefox
+    app.camera.fov += event.detail * 1.0;
+  }
 
-window.addEventListener("resize", function(){
+  app.camera.fov = Math.max(40, Math.min(100, app.camera.fov));
+  app.camera.updateProjectionMatrix();
+}
+
+window.addEventListener('mousewheel', onMouseWheel, false);
+window.addEventListener('DOMMouseScroll', onMouseWheel, false);
+
+window.addEventListener("resize", function () {
 
   //if we change the window all inside will change appropriately
   app.width = window.innerWidth;
@@ -87,25 +88,4 @@ window.addEventListener("resize", function(){
   app.renderer.setSize(app.width, app.height);
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
